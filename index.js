ca29f4ce3e8524a05187af151fb3199b030978df
Aksh202004 <125826202+Aksh202004@users.noreply.github.com>
2023-10-09 20:41:29 +0530

    l

--- index.js
+++ index.js
@@ -2,18 +2,31 @@ import express from "express";
 import { dirname } from "path";
 import { fileURLToPath } from "url";
 import { log } from "console";
-
+import mongoose from "mongoose";
 import bodyParser from "body-parser";
 
 const app = express();
 const port = 3000;
+
+const userSchema = new mongoose.Schema ({
+    name: String,
+    phoneno: String,
+    from: String,
+    to: String,
+    seatswanted: Number,
+
+
+  });
+const User = new mongoose.model("User", userSchema); 
+mongoose.connect('mongodb+srv://jj:1Million$@cluster0.msnztf8.mongodb.net/avyaan?retryWrites=true&w=majority').then(console.log("connected"));
+
 
 
 // app.use(express.json());
 // app.use(express.urlencoded({ extended: true }));
 
 let users=[]
- 
+ var AV = "" ;// ...................................support variable
 app.set('view engine', 'ejs');
 
 app.use(express.static('public'))  
@@ -38,6 +51,8 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.post('/adduser',  (req,res) =>{
   const user = req.body.name;
   console.log(req.body);
+
+ AV= req.body.phoneno;  //..........................initialized
   // res.send(req.body)
   const userdata = {
     "name": req.body.name,
@@ -46,11 +61,33 @@ app.post('/adduser',  (req,res) =>{
     "to": req.body.to,
     "date":Date.now().toString(),
   }
+ // here..........................................................
+ User.insertMany({ 
+        name: req.body.name,
+        phoneno:req.body.phoneno,
+      from: req.body.from,
+    to: req.body.to,
+    date:Date.now().toString()
+        });
+
+
+ 
   console.log(userdata);
   users.push(userdata);
   console.log(user);
   res.send('User added successfully'); // You should send a response to the client
 
+})
+
+
+app.get("/view" , async(req,res)=>{
+const jj = await User.find({phoneno : AV});
+ if(jj.length === 0){
+  res.redirect("/choose");
+ }
+// jj[0] is the user object not any array..........
+  res.render('view',{ user: jj[0] });
+ 
 })
 
 app.get("/", auth, (req, res) => {
